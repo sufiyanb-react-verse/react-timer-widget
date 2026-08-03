3c65f5f59fd91393328125fcfc763507106a179b
sufi-webverse <sufiyanbudye7@gmail.com>
2026-08-03 10:19:44 +0530

    added widget div placement

--- src/widget.jsx
+++ src/widget.jsx
@@ -3,30 +3,30 @@ import { createRoot } from "react-dom/client";
 import Timer from "./Timer";
 
 const TimerWidget = {
-  mount(selector) {
-    const container =
-      typeof selector === "string"
-        ? document.querySelector(selector)
-        : selector;
+//   mount(selector) {
+//     const container =
+//       typeof selector === "string"
+//         ? document.querySelector(selector)
+//         : selector;
 
-    if (!container) {
-      throw new Error("TimerWidget: container not found");
-    }
+//     if (!container) {
+//       throw new Error("TimerWidget: container not found");
+//     }
 
-    const root = createRoot(container);
+//     const root = createRoot(container);
 
-    root.render(
-      <React.StrictMode>
-        <Timer />
-      </React.StrictMode>
-    );
+//     root.render(
+//       <React.StrictMode>
+//         <Timer />
+//       </React.StrictMode>
+//     );
 
-    return {
-      unmount() {
-        root.unmount();
-      }
-    };
-  },
+//     return {
+//       unmount() {
+//         root.unmount();
+//       }
+//     };
+//   },
   mount(options = {}) {
     // Create container
     const container = document.createElement("div");
